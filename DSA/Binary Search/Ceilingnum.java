f9f1bed7b0405483f2ddc66f9d2aac624259345f
gopi374 <an3201088@gmail.com>
2026-01-24 16:11:38 +0530

    Initial commit

--- DSA/Binary Search/Ceilingnum.java
+++ DSA/Binary Search/Ceilingnum.java
@@ -1,20 +1,18 @@
 public class Ceilingnum {
     public static void main(String[] args) {
-    //ceiling is stand for a number is greater or = to target; it follow(>=)  approch in Binary search
+    //ceiling is stand for a smallest number in array greater or = to target; it follow(>=)  approch in Binary search 
+    // if target = 19 , output=50
         int[] arr={10,15,19,50,52,85};
-        int target = 19;
+        int target = 0;
         System.out.println(ceiling(arr, target));
     }
     static int ceiling(int[] arr, int target){
-        for(int i = 0; i< arr.length-1; i++){
+        for(int i = 0; i< arr.length; i++){
             if(arr[i]>=target){
-                System.out.println("Element found at "+i);
-            }
-            else{
-                System.out.println("not found");
+                return arr[i];
             }
         }
-        return 0;
+        return arr[0];
     }
 
 }
---
+++ DSA/Binary Search/tempCodeRunnerFile.java
@@ -0,0 +1,3 @@
+if(arr[i]<=arr[i+1]){
+                //     System.out.println(arr[i]);
+                // }
