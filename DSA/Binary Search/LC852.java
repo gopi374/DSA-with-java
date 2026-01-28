e831f97e777bf9553cd77475829f2ea2cdcfca4e
gopi374 <an3201088@gmail.com>
2026-01-28 23:05:35 +0530

    done

--- DSA/Binary Search/LC852.java
+++ DSA/Binary Search/LC852.java
@@ -1,19 +1,25 @@
 public class LC852 {
     public static void main(String[] args) {
         int[] arr={0,1,2,1,0};
-        System.out.println(peak(arr));
+        // System.out.println(peak(arr));
+        System.out.println(max(arr));
+        System.out.println(arr.length);
         
     }
-    // static int peak(int[] arr){
-    //     for(int i=0;i<arr.length;i++){
-    //         boolean leftok = (i == 0) || (arr[i]>=arr[i-1]);
-    //         boolean rightok = (i == arr.length-1) || (arr[i]>=arr[i+1]);
-    //         if(leftok && rightok){
-    //             return arr[i];
-    //         }
-    //     }
-    //     return -1;
-    // }
+    static int max(int[] arr){
+        int start = 0;
+        int end = arr.length-1;
+        while(start<end){
+            int mid =start+(end-start)/2;
+            if(arr[mid]>arr[mid+1]){
+                end =mid;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return start;
+    }
     static int peak(int[] arr) {
     for (int i = 0; i < arr.length; i++) {
         // Check left neighbor
