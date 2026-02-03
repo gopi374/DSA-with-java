ac96e7f729b37f9d24230ac5107e94f37abb4f8b
gopi374 <an3201088@gmail.com>
2026-02-03 23:52:06 +0530

    New patterns are added

--- DSA/Patterns/pattern.java
+++ DSA/Patterns/pattern.java
@@ -157,22 +157,23 @@ public class pattern {
     static void pattern8(int n) {
 
                            /*
-        * * * * *
-        *       *
-        *       *
-        *       *
-        * * * * *
+         *  *  *  *  *  *  * 
+            *  *  *  *  *
+               *  *  *
+                  *
                       */
 
         for (int row = 0; row < n; row++) {
-            for (int col = 0; col < row-1; col++) {
-                System.out.print(" ");
+            for (int col = 0; col < row; col++) {
+                System.out.print("   ");
             }
-            for(int col =0;col<2*n-row;col++){
-                System.out.print("*");
+            for(int col =0;col<2*(n-row)-1;col++){
+                System.out.print(" * ");
             }
             System.out.println();
         }
     }
+    
+    
 
 }
