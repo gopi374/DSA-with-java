4284ae2fff1013a6d414e542c82d029e556ee5b4
gopi374 <an3201088@gmail.com>
2026-02-03 22:48:51 +0530

    patterns are added

--- DSA/Patterns/pattern.java
+++ DSA/Patterns/pattern.java
@@ -2,8 +2,10 @@
 public class pattern {
     public static void main(String[] args) {
         int n = 4;
-        pattern1(n);
-        pattern2(n);
+        // pattern1(n);
+        // pattern2(n);
+        // pattern3(n);
+        pattern4(n);
     }
 
     static void pattern1(int n) {
@@ -41,4 +43,48 @@ public class pattern {
      *
       
      */
+
+    static void pattern3(int n) {
+
+        /*
+               *
+             * *
+           * * *
+         * * * *
+                 */
+        for (int row = 0; row < n; row++) {
+            //space
+            for (int col = 0; col < n- row-1; col++) {
+                System.out.print("  ");
+            }
+            //star
+            for(int col=0;col<row+1;col++){
+                System.out.print(" *"); 
+            }
+            System.out.println();
+        }
+    }
+
+
+    static void pattern4(int n) {
+
+        /*
+         * * * *
+           * * *
+             * *
+               *
+                    */
+        for (int row = 0; row < n; row++) {
+            //space
+            for (int col = 0; col < row; col++) {
+                System.out.print("  ");
+            }
+            //star
+            for(int col=0;col<n-row;col++){
+                System.out.print(" *"); 
+            }
+            System.out.println();
+        }
+    }
+
 }
