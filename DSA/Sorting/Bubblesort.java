4cad293a1b350e82204cf78cc949056b4cf39cc7
gopi374 <an3201088@gmail.com>
2026-02-08 22:00:18 +0530

    initial commits

---
+++ DSA/Sorting/Bubblesort.java
@@ -0,0 +1,23 @@
+
+public class Bubblesort {
+    public static void main(String[] args) {
+        int[] arr={2,0,1};
+        bubbleSort(arr);
+        System.out.println(java.util.Arrays.toString(arr));
+    }
+    static void bubbleSort(int[] arr) {
+
+        // it solves LC problem problem 75
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
