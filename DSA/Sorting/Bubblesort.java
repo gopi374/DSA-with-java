e84ee73f57b9aedfd836e412e0a007e49d694bd9
gopi374 <an3201088@gmail.com>
2026-02-09 20:05:04 +0530

    new changes

--- DSA/Sorting/Bubblesort.java
+++ DSA/Sorting/Bubblesort.java
@@ -1,7 +1,7 @@
 
 public class Bubblesort {
     public static void main(String[] args) {
-        int[] arr={2,0,1};
+        int[] arr={0,1,2};
         bubbleSort(arr);
         System.out.println(java.util.Arrays.toString(arr));
     }
@@ -9,6 +9,7 @@ public class Bubblesort {
 
         // it solves LC problem problem 75
         int n = arr.length;
+        boolean swapped=false;
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
@@ -16,8 +17,13 @@ public class Bubblesort {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
+                    swapped=true;
+                    
                 }
             }
+        }
+        if(!swapped){
+            System.out.println("Array is already sorted");
         }
     }
 }
---
+++ DSA/Sorting/happynumberLC202.java
@@ -0,0 +1,12 @@
+import java.math.*;
+class happynumberLC202{
+    public static void main(String[] args) {
+        int n=19;
+        System.out.println(pow(n,2));
+        // while(n<=1){
+        //     int remind=n/2;
+        //     n=n/2;
+            
+        // }
+    }
+}
