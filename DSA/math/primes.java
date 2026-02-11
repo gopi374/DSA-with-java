ea706da1b3bf74c12c110067f835f66384597120
gopi374 <an3201088@gmail.com>
2026-02-11 20:03:36 +0530

    new changes

---
+++ DSA/math/primes.java
@@ -0,0 +1,22 @@
+public class primes {
+    public static void main(String[] args) {
+        int n=10;
+        for(int i=2;i<=n;i++){
+            if(isprime(i)){
+                System.out.print(i+" ");
+            }
+        }
+
+    }
+    static boolean isprime(int n){
+        if(n<=1){
+            return false;
+        }
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
