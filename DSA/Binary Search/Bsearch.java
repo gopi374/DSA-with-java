f67ce5c8de4db13a32a280c1b917254c03d79b91
gopi374 <an3201088@gmail.com>
2026-02-14 00:56:58 +0530

    Update Bsearch.java

--- DSA/Binary Search/Bsearch.java
+++ DSA/Binary Search/Bsearch.java
@@ -1,7 +1,7 @@
 public class Bsearch {
     public static void main(String[] args) {
-        int[] arr = {4,5,6,7,0,1,2};
-        int target = 0;
+        int[] arr = {5,7,7,8,8,10};
+        int target = 8;
 
         if (check(arr)) {
             System.out.println(Bsearch(arr, target));
