b21a5bc14e94c0e0e14b3321be4299d59f169e17
gopi374 <an3201088@gmail.com>
2026-02-22 20:39:53 +0530

    new changes

--- DSA/Two pointers/removeDuplicate.java
+++ DSA/Two pointers/removeDuplicate.java
@@ -1,9 +1,9 @@
 public class removeDuplicate {
     public static void main(String[] args) {
-        int[] nums={0,0,1,1,1,2,2,3,3,4};
+        int[] nums={1,1,2};
         System.out.println(remov(nums));
     }
-    
+
     static int remov(int[]nums){
         int i=0;
             for(int j=1;j<nums.length;j++){
