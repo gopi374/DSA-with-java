78de747f832a9b6b6a3908505c7ccd9f44c7987b
gopi374 <an3201088@gmail.com>
2026-02-26 00:15:24 +0530

    new changes

--- DSA/Two pointers/Minimumoftwoarray.java
+++ DSA/Two pointers/Minimumoftwoarray.java
@@ -2,38 +2,12 @@ public class Minimumoftwoarray {
     public static void main(String[] args) {
         int[] nums1={4,12,13};
         int[] nums2={4,2};
-        // int min=nums1[0];
-        // for(int i=0;i<nums1.length;i++){
-        //     if(nums1[i]<min){
-        //         min=nums1[i];
-        //     }
-        // }
-        // for(int i=0;i<nums2.length;i++){
-        //     if(nums2[i]<min){
-        //         min=nums2[i];
-        //     }
-        // }
+        //output 4
         System.out.println(mini(nums1, nums2));
     }
     static int mini(int[] nums1,int[] nums2){
-        int min=nums1[0];
-        for(int i=0;i<nums1.length;i++){
-            if(nums1[i]<min){
-                min=nums1[i];
-            }
-        }
-        int min2=nums2[0];
-        for(int i=0;i<nums2.length;i++){
-            if(nums2[i]<min){
-                min2=nums2[i];
-            }
-        }
-        if(min<min2){
-            min=min+1;
-        }else if(min2<min){
-            min2=min2+1;
-        }
-        return Math.min(min, min2);
-        //output is 4
+        int i=0;
+        int j=0;
+        return -1;
     }
 }
