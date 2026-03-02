35bb296da8bf6c995f2f883ee26346bfd4d87c36
gopi374 <an3201088@gmail.com>
2026-03-03 00:50:13 +0530

    Initial commit

--- DSA/String/Removevowels.java
+++ DSA/String/Removevowels.java
@@ -1,5 +1,18 @@
 class Removevowels{
     public static void main(String[] args) {
-        
+        String s="idea";
+        int n=s.length();
+        int idx=n;
+        for(int i=n-1;i>0;i--){
+            char c=s.charAt(i);
+            if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u'){
+                idx=i;
+            }
+            else{
+                break;
+            }
+        }
+        String result = s.substring(0, idx);
+        System.out.println(result);
     }
 }
