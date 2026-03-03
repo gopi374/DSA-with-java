d203ea99142b3dbeb2286a61155f4b706d6a7dec
gopi374 <an3201088@gmail.com>
2026-03-03 21:53:01 +0530

    new changes

--- DSA/Transpose/RotateImage.java
+++ DSA/Transpose/RotateImage.java
@@ -1,17 +1,15 @@
 class RotateImage {
     public static void main(String[] args) {
         int[][] matrix = {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9}
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
         };
-        int n = matrix.length;
-        System.out.println(n);
         rotate(matrix);
     }
+
     public static void rotate(int[][] matrix) {
         int n = matrix.length;
-         
 
         // Step 1: Transpose the matrix
         for (int i = 0; i < n; i++) {
@@ -21,19 +19,28 @@ class RotateImage {
                 matrix[j][i] = temp;
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                System.out.print(matrix[i][j]+" ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
             }
             System.out.println("\n");
         }
         // Step 2: Reverse each row
-        // for (int i = 0; i < n; i++) {   
-        //     for (int j = 0; j < n / 2; j++) {
-        //         int temp = matrix[i][j];
-        //         matrix[i][j] = matrix[i][n - 1 - j];
-        //         matrix[i][n - 1 - j] = temp;
-        //     }
-        // }
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+            while (left <= right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println("\n");
+        }
     }
 }
