38c30c1e7b6283cfb1fc06ccda9e5f24d0804563
gopi374 <an3201088@gmail.com>
2026-03-06 21:35:18 +0530

    new changes

--- DSA/String/contiguoussegmentofones.java
+++ DSA/String/contiguoussegmentofones.java
@@ -1,16 +1,20 @@
 import java.util.*;
 class contiguoussegmentofones{
     public static void main(String[] args) {
-        String s="110";
+        String s="1101";
         System.out.println(checkOnesSegment(s));
     }
     public static boolean checkOnesSegment(String s) {
-        int n=s.length();
-        for(int i=0;i<n;i++){
-            if((s.charAt(i)==1) || i<n && s.charAt(i+1)==1 ){
-                return true;
+        boolean zero=false;
+        for(int i=0;i<s.length()-1;i++){
+            if(s.charAt(i)=='0'){
+                zero=true;
+            }
+            else if(zero){
+                return false;
             }
         }
-        return false;
+        return true;
+
     }
 }
