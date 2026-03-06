b0753c7927fa22ad45f0c525293e1a13cb6172e7
gopi374 <an3201088@gmail.com>
2026-03-06 21:55:11 +0530

    new changes

--- DSA/String/contiguoussegmentofones.java
+++ DSA/String/contiguoussegmentofones.java
@@ -1,7 +1,9 @@
 import java.util.*;
 class contiguoussegmentofones{
     public static void main(String[] args) {
-        String s="1101";
+        String s="1101"; 
+        //output = true -because segments only checks '01' if it is present in our input then return false;
+        // test cases - "1000" true 
         System.out.println(checkOnesSegment(s));
     }
     public static boolean checkOnesSegment(String s) {
