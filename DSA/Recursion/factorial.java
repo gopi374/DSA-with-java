df2321ef5c6e89b64b4841d74d680e41fc55d4e6
gopi374 <an3201088@gmail.com>
2026-03-10 01:14:31 +0530

    new changes

--- DSA/Recursion/factorial.java
+++ DSA/Recursion/factorial.java
@@ -1,12 +1,27 @@
 public class factorial {
     public static void main(String[] args) {
-        int n=5;
-        System.out.println(factorial(n));
+        int n=1342;
+        // System.out.println(factorial(n));
+        // funrev(n);
+        System.out.println(sumofnum(n));
     }
     static int factorial(int n){
         if(n==0 || n==1){
             return 1;
         }
         return factorial(n-1)*n;
+    }
+    static void funrev(int n){
+        if(n==0){
+            return;
+        }
+        funrev(n-1);
+        System.out.print(n+" ");
+    }
+    static int sumofnum(int n){
+        if(n==0){
+            return 0;
+        }
+        return sumofnum(n/10)+(n%10);
     }
 }
