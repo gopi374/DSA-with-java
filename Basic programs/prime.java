8213d9a41a46387184eca84c6a02098e696b5a52
gopi374 <an3201088@gmail.com>
2026-03-09 21:14:44 +0530

    record local deletions before rebase

--- Basic programs/prime.java
+++ Basic programs/prime.java
@@ -1,11 +1,16 @@
 public class prime {
     public static void main(String[] args) {
+        
         int n =23;
+        int[] arr= new int[n];
         System.out.println(isprime(n)); // it will only print n is prime or not
         for(int i=2;i<n;i++){
             if(isprime(i)){
                 System.out.print(i+" "); // it will print those numbers are prime or less than n
             }
+        }
+        for(int num :arr){
+            System.out.print(num+" ");
         }
     }
     static boolean isprime(int n){
