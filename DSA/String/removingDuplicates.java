ed19aa19d9c6edd1da20d85a825c6be0f416d794
gopi374 <an3201088@gmail.com>
2026-03-22 21:59:05 +0530

    new changes

--- DSA/String/removingDuplicates.java
+++ DSA/String/removingDuplicates.java
@@ -4,7 +4,7 @@ public class removingDuplicates {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-        
+        String lowertxt = s.toLowerCase();
         StringBuilder sb =new StringBuilder();
     }
 }
