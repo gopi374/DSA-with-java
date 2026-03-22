0d39e8289cdcfe29ea9ddde65b5d36497be5d32d
gopi374 <an3201088@gmail.com>
2026-03-22 22:30:13 +0530

    new changes

--- DSA/String/removingDuplicates.java
+++ DSA/String/removingDuplicates.java
@@ -3,14 +3,19 @@ import java.util.Scanner;
 public class removingDuplicates {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine(); //
-        StringBuilder sb =new StringBuilder();
+        String s = sc.nextLine();
+        if (s.length() == 0) return;
+
+        StringBuilder sb = new StringBuilder();
+        
         sb.append(s.charAt(0));
+
         for (int i = 1; i < s.length(); i++) {
-            if(s.charAt(1) != s.charAt(i-1)){
+            if (s.charAt(i) != s.charAt(i - 1)) {
                 sb.append(s.charAt(i));
             }
         }
+        
         System.out.println(sb.toString());
     }
 }
