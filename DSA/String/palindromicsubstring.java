a4514fd97892c0ca70dc157c2802210b744f2327
gopi374 <an3201088@gmail.com>
2026-03-22 22:42:11 +0530

    new changes

--- DSA/String/palindromicsubstring.java
+++ DSA/String/palindromicsubstring.java
@@ -8,7 +8,7 @@ public class palindromicsubstring {
 
         for (int i = 0; i < s.length(); i++) {
             count += countPal(s, i, i);     
-            count += countPal(s, i, i + 1); // even-length palindromes
+            count += countPal(s, i, i + 1); // even-length palindr
         }
 
         System.out.println(count);
