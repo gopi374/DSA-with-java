04a8db0f3177c3e4a9d4fffb5a993f1af7d56934
gopi374 <an3201088@gmail.com>
2026-03-22 21:01:34 +0530

    new changes

---
+++ DSA/String/evenindexcheck.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class evenindexcheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        StringBuilder sb =new StringBuilder();
+        for(int i = 0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(i%2==0){
+                sb.append((char)(ch+1));
+            }
+            else{
+                sb.append((char)(ch-1));
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
