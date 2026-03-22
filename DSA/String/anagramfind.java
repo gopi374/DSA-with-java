dc6f86c8817ae15248a813091a1d1541c3939242
gopi374 <an3201088@gmail.com>
2026-03-22 22:12:25 +0530

    new changes

--- DSA/String/anagramfind.java
+++ DSA/String/anagramfind.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class anagramfind {
@@ -5,10 +6,21 @@ public class anagramfind {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
         String t = sc.nextLine();        
-        System.out.println(anagram(s,t));
+        System.out.println(anagram(s, t));
     }
-    static String anagram(String s ,String t){
-        
-        return t;
+
+    static String anagram(String s, String t) {
+        if (s.length() != t.length()) 
+            return "false";
+
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        if (Arrays.equals(a, b)) 
+            return "true";
+        return "false";
     }
 }
