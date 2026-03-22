e51bcc12971730966b89285fafffe411ad796c0e
gopi374 <an3201088@gmail.com>
2026-03-22 21:35:57 +0530

    new changes

--- DSA/String/ispalindrone.java
+++ DSA/String/ispalindrone.java
@@ -4,20 +4,11 @@ public class ispalindrone {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-        StringBuilder sb =new StringBuilder(s);
-        for(int i =0; i<s.length();i++){
-            char ch = s.charAt(i);
-            sb.append(iscase(ch));
-        }
+        String lower = s.toLowerCase();
+        StringBuilder sb =new StringBuilder(lower);
         sb.reverse();
-        System.out.println((sb.toString()).equals(s));
+        System.out.println(sb.toString());
+        System.out.println((sb.toString()).equals(lower));
     }
-    static char iscase(char a){
-		if(Character.isUpperCase(a)){
-			return Character.toLowerCase(a);
-		}
-		else{
-			return Character.toUpperCase(a);
-		}
-	}
+    
 }
