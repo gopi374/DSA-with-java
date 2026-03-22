14f34c45557acaaa34a0891fe8fa4823c35e29cf
gopi374 <an3201088@gmail.com>
2026-03-22 21:30:18 +0530

    new changes

--- DSA/String/ispalindrone.java
+++ DSA/String/ispalindrone.java
@@ -5,6 +5,10 @@ public class ispalindrone {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
         StringBuilder sb =new StringBuilder(s);
+        for(int i =0; i<s.length();i++){
+            char ch = s.charAt(i);
+            ch.toLowerCase();
+        }
         sb.reverse();
         System.out.println((sb.toString()).equals(s));
     }
