1995fa6e744f13f1c873b35d24e611cc617367b6
gopi374 <an3201088@gmail.com>
2026-03-23 23:36:41 +0530

    new changes

--- DSA/Transpose/AnticlockwiseSpiralMatrix.java
+++ DSA/Transpose/AnticlockwiseSpiralMatrix.java
@@ -1,52 +1,51 @@
 import java.util.*;
-
-class AnticlockwiseSpiralMatrix {
+class AnticlockwiseSpiralMatrix{
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        int[][] matrix = new int[4][4];
-        for(int i=0;i<4;i++){
-            for(int j=0;j<4;j++){
-                matrix[i][j]=in.nextInt();
+        Scanner sc= new Scanner(System.in);
+        int m=sc.nextInt();
+        int n =sc.nextInt();
+        int[][] mat = new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j = 0; j<n;j++){
+                mat[i][j]=sc.nextInt();
             }
         }
-        List<Integer> res=new ArrayList<>();
-        res=AnticlockwiseSpiral(matrix);
-        for (int i = 0; i < res.size(); i++) {
-            System.out.print(res.get(i) + ", ");
+        List<Integer> res= result(mat);
+
+        for(int i=0;i<res.size();i++){
+           
+            System.out.print(res.get(i)+", ");
         }
         System.out.print("END");
+
     }
-
-    public static List<Integer> AnticlockwiseSpiral(int[][] matrix) {
-        List<Integer> res = new ArrayList<>();
-
-        int sr = 0, sc = 0;
-        int er = matrix.length - 1;
-        int ec = matrix[0].length - 1;
-
-        while (sr <= er && sc <= ec) {
-            for (int row = sr; row <= er; row++) {
-                res.add(matrix[row][sc]);
+    public static List<Integer> result(int[][] mat){
+        List<Integer> res=new ArrayList<>();
+        int sr=0,sc=0;
+        int er=mat.length-1,ec=mat[0].length-1;
+        while(sc<=ec && sr<=er){
+            for(int row = sr;row<=er;row++){
+                res.add(mat[row][sc]);
             }
             sc++;
-            for (int col = sc; col <= ec; col++) {
-                res.add(matrix[er][col]);
+            for(int col=sc;col<=ec;col++){
+                res.add(mat[er][col]);
             }
             er--;
-            if (sc <= ec) {
-                for (int row = er; row >= sr; row--) {
-                    res.add(matrix[row][ec]);
+            if(sc<=ec){
+                for(int row=er;row>=sr;row--){
+                    res.add(mat[row][ec]);
                 }
                 ec--;
-            }   
-            if (sr <= er) {
-                for (int col = ec; col >= sc; col--) {
-                    res.add(matrix[sr][col]);
+            }
+            if(sr<=er){
+                for(int col=ec;col>=sc;col--){
+                    res.add(mat[sr][col]);
                 }
                 sr++;
             }
-        }
 
+        }
         return res;
     }
 }
