6056c7492e028ab6a1e08000f69c8da109309fe2
gopi374 <an3201088@gmail.com>
2026-03-23 20:33:37 +0530

    new changes

--- DSA/Transpose/waveprintcolwise.java
+++ DSA/Transpose/waveprintcolwise.java
@@ -14,12 +14,12 @@ public class waveprintcolwise {
         for(int i=0;i<col;i++){
             if(i%2==0){
                 for(int j=0;j<row;j++){
-                    System.out.print(mat[i][j]+", ");
+                    System.out.print(mat[j][i]+", ");
                 }
             }
             else{
                 for(int j =row-1;j>=0;j--){
-                    System.out.print(mat[i][j]+", ");
+                    System.out.print(mat[j][i]+", ");
                 }
             }
         }
