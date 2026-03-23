ee95ce19e95ab741dda6b4b4f16e13dd074167cb
gopi374 <an3201088@gmail.com>
2026-03-23 20:11:12 +0530

    new changes

--- DSA/String/anagramfind.java
+++ DSA/String/anagramfind.java
@@ -5,13 +5,23 @@ public class anagramfind {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-        String t = sc.nextLine();        
-        System.out.println(anagram(s, t));
+        String t = sc.nextLine();
+        char[] a = s.toCharArray();
+        Arrays.sort(a);
+        for (Object elem : a) {
+            System.out.println(elem+" ");
+        }        
+        char[] b = t.toCharArray();
+        Arrays.sort(b);
+        for (Object elem : b) {
+            System.out.println(elem+" ");
+        } 
+        // System.out.println(anagram(s, t));
     }
 
-    static String anagram(String s, String t) {
+    static boolean anagram(String s, String t) {
         if (s.length() != t.length()) 
-            return "false";
+            return false;
 
         char[] a = s.toCharArray();
         char[] b = t.toCharArray();
@@ -20,7 +30,7 @@ public class anagramfind {
         Arrays.sort(b);
 
         if (Arrays.equals(a, b)) 
-            return "true";
-        return "false";
+            return true;
+        return false;
     }
 }
