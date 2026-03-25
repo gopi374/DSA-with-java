d2f2635e8dd6ff34ad35b2405de36640efd675a2
gopi374 <an3201088@gmail.com>
2026-03-25 20:25:26 +0530

    new changes

---
+++ DSA/math/HCF.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class HCF {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int hcf=0;
+        int min = Integer.min(a,b); 
+        for(int i=min;i>=0;i--){
+            if(a%i==0 && b%i==0){
+                hcf=i;
+                break;
+            };
+        }
+        System.out.print(hcf+" ");
+    }
+}
