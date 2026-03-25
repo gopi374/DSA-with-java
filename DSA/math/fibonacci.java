13f04a571e1fa894dd6e40a07d30a1d4b4bca635
gopi374 <an3201088@gmail.com>
2026-03-25 19:45:25 +0530

    new changes

---
+++ DSA/math/fibonacci.java
@@ -0,0 +1,11 @@
+public class fibonacci {
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(fibo(n));
+    }
+    static int fibo(int n){
+        if(n<=1)
+            return n;
+        return fibo(n-1)+fibo(n-2);
+    }
+}
