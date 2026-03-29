84bdaa7d9d56293f6a1b69d062335bfcdee5bf2e
gopi374 <an3201088@gmail.com>
2026-03-29 20:41:00 +0530

    new changes

--- DSA/Arrays/klargestarray.java
+++ DSA/Arrays/klargestarray.java
@@ -2,14 +2,18 @@ import java.util.*;
 class klargestarray {
     public static void main(String[] args) {
         int[] nums ={3,2,3,1,2,4,5,5,6};
+        Arrays.sort(nums);
+        int k=4;
         for (int i : nums) {
             System.out.print(i+" ");
         }
-        System.out.println(klar(nums));
+        System.out.println(klar(nums,k));
     }
-    static int klar(int[] nums){
+    static int klar(int[] nums,int k){
         Arrays.sort(nums);
-        return nums[]
-        return k;
+        int ans=0;
+        for(int i=nums.length-1;i>k;i--)
+            ans=nums[i];
+        return ans;
     }
 }
