425fd4b1feedd42139f4af931261f6e095996d47
gopi374 <an3201088@gmail.com>
2026-03-29 17:32:49 +0530

    new commit

--- DSA/Arrays/containduplicate.java
+++ DSA/Arrays/containduplicate.java
@@ -1,3 +1,16 @@
+import java.util.*;
 class containduplicate{
-    psv
+    public static void main(String[] args) {
+        int[] nums={1,2,3,4,1};
+        System.out.println(contains(nums));
+    }
+    static boolean contains(int[] nums){
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i++){
+            if(nums[i-1]==nums[i])
+                return true;
+        }
+        return false;
+
+    }
 }
