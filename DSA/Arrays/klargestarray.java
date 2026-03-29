0cc30454ec5d6f3360961e122f2e5066ea401db7
gopi374 <an3201088@gmail.com>
2026-03-29 17:39:26 +0530

    new commit

---
+++ DSA/Arrays/klargestarray.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class klargestarray {
+    public static void main(String[] args) {
+        int[] nums ={3,2,3,1,2,4,5,5,6};
+        for (int i : nums) {
+            System.out.print(i+" ");
+        }
+        System.out.println(klar(nums));
+    }
+    static int klar(int[] nums){
+        Arrays.sort(nums);
+        return nums[]
+        return k;
+    }
+}
