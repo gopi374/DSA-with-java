038b28d9ca07cc8d5dbb9622cf2c4a474a5275b7
gopi374 <an3201088@gmail.com>
2026-04-18 02:39:40 +0530

    new changes

--- DSA/Recursion/listreturn.java
+++ DSA/Recursion/listreturn.java
@@ -5,16 +5,27 @@ public class listreturn {
     public static void main(String[] args) {
         int[] arr={1,2,5,6,6,8,91,0};
         ArrayList<Integer> list=new ArrayList<>();
-        System.out.println(find(arr, 6, 0, list)); //output = 3,4
+        System.out.println(find(arr, 6, 0)); //output = 3,4
 
     }
 
-    static ArrayList<Integer> find(int[] arr,int target,int i, ArrayList<Integer> list){
+    static ArrayList<Integer> find(int[] arr,int target,int i, ArrayList<Integer> list){ //returning list using arguments
         if(i==arr.length)
             return list;
         if(arr[i]==target){
             list.add(i);
         }    
         return find(arr, target, i+1,list);
+    }
+
+    static ArrayList<Integer> find(int[] arr,int target,int i){ //returning list using no arguments
+        ArrayList<Integer> list =new ArrayList<>();
+        while (i<arr.length) { 
+            if(arr[i]==target){
+                list.add(i);
+            } 
+            i++;
+        }
+        return list;
     }
 }
