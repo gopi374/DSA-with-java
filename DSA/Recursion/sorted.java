322fcb588fc6694294bd66ff5bb3cbcb8f19a6f9
gopi374 <an3201088@gmail.com>
2026-04-17 19:26:33 +0530

    new changes

--- DSA/Recursion/sorted.java
+++ DSA/Recursion/sorted.java
@@ -1,6 +1,6 @@
 class sorted{
     public static void main(String[] args) {
-        int[] arr = {1,2,5,6,9,7,10};
+        int[] arr = {1,2,5,6,9,10};
         System.out.println(isSorted(arr, 0));
     }
     static boolean isSorted(int[] arr, int idx){
