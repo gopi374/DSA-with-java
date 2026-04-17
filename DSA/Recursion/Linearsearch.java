c48a9fa87b8546d7161c01b04bd0dd35ea8f0506
gopi374 <an3201088@gmail.com>
2026-04-18 00:31:13 +0530

    new changes

--- DSA/Recursion/Linearsearch.java
+++ DSA/Recursion/Linearsearch.java
@@ -3,14 +3,27 @@ public class Linearsearch {
         int[] arr = {1,5,3,9,45,32,69};
         int target = 32;
         System.out.println(LS(arr,target,0));
+        System.out.println(LSint(arr, target, 0));
     }
     static boolean LS(int[] arr, int target,int i){
         // if(i == arr.length){
         //     return false;
         // }
         if(arr[i]==target){
-            System.out.println(i);
+            return true;
         }
-        return LS(arr, target, i+1);
+        return arr[i]==target || LS(arr, target, i+1);
+    }
+    static int LSint(int[] arr,int target,int i){
+        if(i==arr.length){
+            return -1;
+        }
+        if(arr[i]==target){
+            return i;
+        }
+        else{
+           return LSint(arr, target, i+1);
+        }
+        
     }
 }
