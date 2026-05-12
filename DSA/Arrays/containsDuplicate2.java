27e5103574113ddfff655b0a2f60af2982762f07
gopi374 <an3201088@gmail.com>
2026-05-12 20:29:46 +0530

    Fix nested loop condition in containsDuplicate2 to correctly check for duplicates within range

--- DSA/Arrays/containsDuplicate2.java
+++ DSA/Arrays/containsDuplicate2.java
@@ -2,6 +2,16 @@ class containsDuplicate2{
     public static void main(String[] args) {
         int[] nums = {1,2,3,1,2,3};
         int k=3;
-        
+        System.out.println(find(nums,k));
+    }
+    static boolean find(int[] nums, int k){
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;i<nums.length;j++){
+                if(nums[i]==nums[j] && Math.abs(i-j)<=k){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
