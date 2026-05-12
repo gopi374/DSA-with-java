6bae927f23edf08899d3a714c15090e5954f8931
gopi374 <an3201088@gmail.com>
2026-05-12 21:24:42 +0530

    Fix duplicate check logic in containsDuplicate2 to ensure correct index comparison

--- DSA/Arrays/containsDuplicate2.java
+++ DSA/Arrays/containsDuplicate2.java
@@ -1,13 +1,13 @@
 class containsDuplicate2{
     public static void main(String[] args) {
         int[] nums = {1,2,3,1,2,3};
-        int k=3;
+        int k=2;
         System.out.println(find(nums,k));
     }
     static boolean find(int[] nums, int k){
         for(int i=0;i<nums.length;i++){
-            for(int j=0;i<nums.length;j++){
-                if(nums[i]==nums[j] && Math.abs(i-j)<=k){
+            for(int j=0;j<nums.length;j++){
+                if(nums[i]==nums[j] && Math.abs(i-j)<=k && i!=j){
                     return true;
                 }
             }
