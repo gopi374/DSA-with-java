c0b1911b0c94d943deed672ec9ce467c89046f27
gopi374 <an3201088@gmail.com>
2026-05-16 18:55:07 +0530

    Bitwise oprators

--- DSA/math/Bitwise operators/intro.java
+++ DSA/math/Bitwise operators/intro.java
@@ -1,13 +1,25 @@
 class intro{
     public static void main(String[] args) {
         //bitwise AND(&) -  it return true when both the inputs are true, otherwise false.
-        int a = 1,b =0;
+        int a = 5,b =3;
         System.out.println(a&b);
 
         //bitwise OR(|) - it return false when both the inputs are false, otherwise true.
         System.out.println(a | b); 
 
-        // bitwise XOR(^) -  it gives complement of the input when input = 1
-        System.out.println(a^a);
+        // bitwise XOR(^) -  it gives true when only one input is true,
+        System.out.println(a^b);
+
+        //bitwise complement(~) -it gives opesite of input.
+        System.out.println(~b);
+
+        //bitwise left shift(<<) - it shift the bits by one and return digit.
+        //a<<b=a*2^b
+        //a<<1 = it doubles the given digit 
+        System.out.println(4<<1);
+        
+        //bitwise right shift(>>) 
+        //a>>b=a/2^b - it only gives int value
+        System.out.println(a>>b);
     }
 }
