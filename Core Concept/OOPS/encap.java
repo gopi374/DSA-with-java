903a786636af5b39f105bc95d94fa4834fa93cd6
gopi374 <an3201088@gmail.com>
2026-05-29 02:01:57 +0530

    Encapsulation

--- Core Concept/OOPS/encap.java
+++ Core Concept/OOPS/encap.java
@@ -29,13 +29,71 @@ class Student {
     }
 
 }
+class BankAccount{
+    private int accNum;
+    private int password;
+    private int balance;
+    private int deposit;
+    private int withdraw;
 
-class encap {
+    public void setAccNum(int accNum){
+        this.accNum = accNum;
+    }
+    public void setPassword(int password){
+        this.password = password;
+    }
+
+    public void setBalance(int balance){
+
+        this.balance = balance;
+        System.out.println("Initial Balance :"+balance);
+    }
+
+    public int getBalance(){
+        return this.balance;
+    }
+    public void Deposit(int amount){
+        if(amount > 0){
+            balance+=amount;
+            System.out.println("Amount deposited :"+amount);
+            System.out.println("Total Balance :"+balance);
+        }else{
+            System.out.println("Invalid amount");
+        }
+
+    }
+
+    public void withdrew(int amount){
+        if(amount >0 && amount <= balance){
+            balance-=amount;
+            System.out.println("Amount Withdrew :"+amount);
+            System.out.println("Total Balance :"+balance);
+        }else{
+            System.out.println("Insuffiecient Balance");
+        }
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public int getAccNum() {
+        return accNum;
+    }
+
+}
+
+public class encap {
     public static void main(String[] args) {
         Student e = new Student();
         e.setpassword(124536);
         System.out.println(e.getpassword());
         e.setpassword(526341);
         System.out.println(e.getpassword());
+
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.setBalance(2000);
+        bankAccount.Deposit(2500);
+        bankAccount.withdrew(20000);
     }
 }
---
+++ Core Concept/OOPS/inheritance.java
@@ -0,0 +1,6 @@
+public class inheritance {
+    static void main(String[] args) {
+
+
+    }
+}
