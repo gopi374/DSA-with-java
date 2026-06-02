7a87957d71c075defc443a4034b2eba4cff246fb
gopi374 <an3201088@gmail.com>
2026-06-02 16:37:45 +0530

    inheritance

--- Core Concept/OOPS/inheritance.java
+++ Core Concept/OOPS/inheritance.java
@@ -1,6 +1,28 @@
+
+//inheritance refers to acquiring the superclass/parent class properties and methods in subclass
+//inheritance used for code reusability 
+class Grandfather{
+
+    //single level inheritance
+    String name;
+     
+    public void sayHello(){
+        System.out.println(name+": Hellooo");
+    }
+}
+class Parents extends Grandfather{
+
+    public void hii(){
+        System.out.println("Hiii");
+    }
+}
+
+
 public class inheritance {
-    static void main(String[] args) {
-
-
+    public static void main(String[] args) {
+        Parents p = new Parents();
+        p.name="rahul";
+        p.hii();
+        p.sayHello();
     }
 }
