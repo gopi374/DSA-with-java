b8f51191a8836010a153906c2d67939abfcd9ad1
gopi374 <an3201088@gmail.com>
2026-06-03 21:50:42 +0530

    Acccess Modiefiers

--- Core Concept/Access modifiers/intro.java
+++ Core Concept/Access modifiers/intro.java
@@ -6,8 +6,37 @@
 // private -- only accessible in same class within same package
 
 
+class Animal{
+    //public 
+    public void sleep(){
+        System.out.println("Zhzzzzz");
+    }
+    //protected
+    protected void category(){
+        System.out.println("fly");
+    }
+    //private
+    private int age;
+
+    public void setAge(int age){
+        this.age=age;
+    }
+    public int getAge(){
+        return age;
+    }
+}
+class Dog extends Animal{
+    @Override
+    public void sleep(){
+        System.out.println("dog sleeps");
+    }
+}
 class intro{
     public static void main(String args[]){
+        Animal a = new Dog();
+        a.category();
+        a.setAge(5);
+        System.out.println(a.getAge());
 
     }
 }
