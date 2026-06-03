988d76f7404cde6d84bb0b8c982e2e1d38282077
gopi374 <an3201088@gmail.com>
2026-06-03 21:43:42 +0530

    intro -- access modier

---
+++ Core Concept/Access modifiers/intro.java
@@ -0,0 +1,13 @@
+//Access modifiers - are used to controls the code visibility(scope) means we can define the accessebility of  classes , varialbe, methods . 
+//types of Access modifiers - 
+// public -- it can access from anywhere in program (in same packege or outside)
+// protected -- it is only accessible in same package by subclass, it is mostly used in inheritance to allow child class used parent class properties.
+// default --(no modifier specifies) Accessible only within the same package.
+// private -- only accessible in same class within same package
+
+
+class intro{
+    public static void main(String args[]){
+
+    }
+}
