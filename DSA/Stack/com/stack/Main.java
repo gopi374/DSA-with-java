81904f99dd7367349929055ceeb07d8a711d5354
gopi374 <an3201088@gmail.com>
2026-06-18 21:56:35 +0530

    Stack

--- DSA/Stack/com/stack/Main.java
+++ DSA/Stack/com/stack/Main.java
@@ -1,4 +1,19 @@
 package com.stack;
 
 public class Main {
+    static void main(String[] args) throws Exception{
+        customStack stack = new customStack(5);
+        stack.push(12);
+//        stack.push(12);
+        stack.push(12);
+        stack.push(12);
+        stack.push(12);
+        System.out.println(stack.push(52));
+    stack.peek();
+        stack.pop();
+//        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+    }
 }
--- DSA/Stack/com/stack/customStack.java
+++ DSA/Stack/com/stack/customStack.java
@@ -1,4 +1,48 @@
 package com.stack;
 
+import static javax.swing.GroupLayout.DEFAULT_SIZE;
+
 public class customStack {
+    protected int[] data;
+    private static final int DEFAULT_SIZE = 0;
+
+    int ptr = -1;
+
+    public  customStack(){
+        this(DEFAULT_SIZE);
+    }
+    public customStack(int size){
+        this.data=new int[size];
+    }
+
+    public  boolean push(int item){
+        if (isFull()){
+            System.out.println("Stack is full ");
+            return false;
+        }
+        ptr++;
+        data[ptr]=item;
+        return true;
+    }
+    public int pop() throws Exception{
+        if (isEmpty()){
+            throw new Exception("cannot remove item");
+        }
+        int removed = data[ptr];
+        ptr--;
+        return removed;
+
+    }
+    public int peek() throws Exception{
+        if(isEmpty()){
+            throw new Exception("cannot get peek element");
+        }
+        return data[ptr];
+    }
+    public boolean isFull(){
+            return ptr == data.length-1;
+    }
+    public boolean isEmpty(){
+        return ptr == -1;
+    }
 }
