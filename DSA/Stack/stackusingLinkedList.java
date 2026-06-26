125e1611d9a3dcd4f3c03206f6427d94c5c73543
gopi374 <an3201088@gmail.com>
2026-06-26 15:25:19 +0530

    Stack Implementation using LinkedList

---
+++ DSA/Stack/stackusingLinkedList.java
@@ -0,0 +1,16 @@
+public class stackusingLinkedList {
+
+    
+
+
+
+
+    private class Node{
+        private int val ;
+        private Node next ;
+
+        public Node(int val){
+            this.val=val;
+        }
+    }
+}
